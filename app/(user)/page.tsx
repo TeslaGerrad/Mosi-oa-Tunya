12a03db942957af02d1408170ec9e3286f6967d4
TeslaGerrad <teslagerrad2@gmail.com>
2023-12-03 22:55:26 +0000

    fixed page.tsx

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,10 @@
-import { previewData } from "next/headers";
 import PreviewSuspense from "../../components/PreviewSuspense";
 import BlogList from "../../components/BlogList";
 import PreviewBlogList from "../../components/PreviewBlogList";
 import { client } from "../../lib/sanity.client";
 import { groq } from "next-sanity";
-import { useEffect } from "react";
-import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
-import UploadData from "../components/ip";
 
+import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
 
 const query = groq`*[_type=='post'] {
    ...,
@@ -17,24 +14,8 @@ const query = groq`*[_type=='post'] {
 `;
 
 export default async function IndexPage() {
-  await UploadData()
-  // await uploadDataToCollection()
-  if (previewData()) {
-    return (
-      <PreviewSuspense
-        fallback={
-          <div role="status">
-            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
-              Loading Preview Data...
-            </p>
-          </div>
-        }
-      >
-        <PreviewBlogList query={query} />
-      </PreviewSuspense>
-    );
-  }
+
 
   const posts = await client.fetch(query);
-  return <BlogList posts={posts} />;
+  return <BlogList posts={posts} />;
 }
