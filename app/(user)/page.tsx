b1ae34b643d5ed921a87c17f72d98fa2a3a458fa
namycodes <namycodes@yahoo.com>
2023-12-04 00:56:07 +0200

    Made updates

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,13 @@
-import { previewData } from "next/headers";
-import PreviewSuspense from "../../components/PreviewSuspense";
+// import { previewData } from "next/headers";
+// import PreviewSuspense from "../../components/PreviewSuspense";
 import BlogList from "../../components/BlogList";
-import PreviewBlogList from "../../components/PreviewBlogList";
+// import PreviewBlogList from "../../components/PreviewBlogList";
 import { client } from "../../lib/sanity.client";
 import { groq } from "next-sanity";
-import { useEffect } from "react";
-import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
+// import { useEffect } from "react";
+// import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
 import UploadData from "../components/ip";
+
 
 
 const query = groq`*[_type=='post'] {
@@ -18,22 +19,6 @@ const query = groq`*[_type=='post'] {
 
 export default async function IndexPage() {
   await UploadData()
-  // await uploadDataToCollection()
-  if (previewData()) {
-    return (
-      <PreviewSuspense
-        fallback={
-          <div role="status">
-            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
-              Loading Preview Data...
-            </p>
-          </div>
-        }
-      >
-        <PreviewBlogList query={query} />
-      </PreviewSuspense>
-    );
-  }
 
   const posts = await client.fetch(query);
   return <BlogList posts={posts} />;
